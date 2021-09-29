b7f74ba276572696c16bd448c9c1b89b7daed12d
Martin Badura <martin.badura@asseco-ce.com>
2021-09-29 12:19:06 +0200

    upraveno dle připomínek

--- projects/01_fyyur/config.py
+++ projects/01_fyyur/config.py
@@ -1,4 +1,7 @@
 import os
+from flask import Flask
+from flask_moment import Moment
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,5 +12,14 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+# IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres:admin@localhost:5432/fyyur'
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+
